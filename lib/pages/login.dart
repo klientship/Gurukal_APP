4a9efcf137c778fe5f62fe56d8bdd55c44cb118d
harshithva <harshith11032001@gmail.com>
2021-02-20 20:57:43 +0530

    user model created

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../controller/login_controller.dart';
-import 'package:dio/dio.dart';
 // import 'signup.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,11 +8,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  bool _secureText = true;
-
   @override
   Widget build(BuildContext context) {
-    getLogin();
     return new Scaffold(
         resizeToAvoidBottomPadding: false,
         body: Column(
@@ -23,75 +19,53 @@ class _LoginPageState extends State<LoginPage> {
               child: Stack(
                 children: <Widget>[
                   Container(
-                    padding: EdgeInsets.fromLTRB(90.0, 110.0, 0.0, 0.0),
-                    child: Text('Login',
+                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                    child: Text('Hello',
                         style: TextStyle(
                             fontSize: 80.0, fontWeight: FontWeight.bold)),
                   ),
-                  // Container(
-                  //   padding: EdgeInsets.fromLTRB(16.0, 175.0, 0.0, 0.0),
-                  //   child: Text('There',
-                  //       style: TextStyle(
-                  //           fontSize: 80.0, fontWeight: FontWeight.bold)),
-                  // ),
-                  // Container(
-                  //   padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
-                  //   child: Text('.',
-                  //       style: TextStyle(
-                  //           fontSize: 80.0,
-                  //           fontWeight: FontWeight.bold,
-                  //           color: Colors.green)),
-                  // ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(16.0, 175.0, 0.0, 0.0),
+                    child: Text('There',
+                        style: TextStyle(
+                            fontSize: 80.0, fontWeight: FontWeight.bold)),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+                    child: Text('.',
+                        style: TextStyle(
+                            fontSize: 80.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green)),
+                  )
                 ],
               ),
             ),
             Container(
-                padding: EdgeInsets.only(top: 25.0, left: 20.0, right: 20.0),
+                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
                 child: Column(
                   children: <Widget>[
-                    TextFormField(
+                    TextField(
                       decoration: InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(),
-                          suffixIcon: Icon(Icons.email)),
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (String value) {
-                        if (value.isEmpty) {
-                          return 'Email is Required';
-                        }
-                        if (!RegExp(
-                                r"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$")
-                            .hasMatch(value)) {
-                          return 'Please Enter valid email address';
-                        }
-                        return null;
-                      },
-
-                      // obscureText: true,
+                          labelText: 'EMAIL',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green))),
                     ),
                     SizedBox(height: 20.0),
-                    TextFormField(
+                    TextField(
                       decoration: InputDecoration(
-                          labelText: 'Password',
-                          border: OutlineInputBorder(),
-                          suffixIcon: IconButton(
-                            icon: Icon(_secureText
-                                ? Icons.remove_red_eye
-                                : Icons.security),
-                            onPressed: () {
-                              setState(() {
-                                _secureText = !_secureText;
-                              });
-                            },
-                          )),
-                      keyboardType: TextInputType.visiblePassword,
-                      validator: (String value) {
-                        if (value.isEmpty) {
-                          return 'Password is Required';
-                        }
-                        return null;
-                      },
-                      obscureText: _secureText,
+                          labelText: 'PASSWORD',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green))),
+                      obscureText: true,
                     ),
                     SizedBox(height: 5.0),
                     Container(
@@ -162,9 +136,4 @@ class _LoginPageState extends State<LoginPage> {
           ],
         ));
   }
-}
-
-void getLogin() async {
-  var response = await Dio().get('https://crm.gurukal.in/api/customers/28');
-  print(response.data);
 }
