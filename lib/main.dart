7aec3779e054f4fb4d934091989a4d88414c8ce2
harshithva <harshith11032001@gmail.com>
2021-08-01 09:33:05 +0530

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:gurukal_app/pages/Quotes.dart';
+import 'package:gurukal_app/pages/dashboard.dart';
 import 'package:gurukal_app/pages/invoice_view.dart';
 import 'package:gurukal_app/pages/login.dart';
 import 'package:gurukal_app/pages/quote_view.dart';
@@ -36,50 +37,36 @@ class MyBottomNavigationBar extends StatefulWidget {
 }
 
 class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
-  int _currentIndex = 0;
+  int currentPage = 0;
+  GlobalKey bottomNavigationKey = GlobalKey();
+
   final List<Widget> _children = [
+    DashboardPage(),
     InvoicePage(),
     QuotePage(),
     TrackPage(),
     UserPage(),
   ];
-  void onTappedBar(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTappedBar,
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.blueAccent[400],
-        unselectedItemColor: Colors.white70,
-        selectedItemColor: Colors.white,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.request_quote_outlined),
-            // ignore: deprecated_member_use
-            title: Text("Invoices"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.analytics),
-            title: Text("Quotes"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_shipping),
-            title: Text("Track"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            title: Text("User"),
-          ),
-        ],
-      ),
-    );
+        body: _children[currentPage],
+        bottomNavigationBar: FancyBottomNavigation(
+          tabs: [
+            TabData(iconData: Icons.home, title: "Dashboard"),
+            TabData(iconData: Icons.request_quote_outlined, title: "Invoices"),
+            TabData(iconData: Icons.analytics, title: "Quotes"),
+            TabData(iconData: Icons.local_shipping, title: "Track"),
+            TabData(iconData: Icons.account_circle, title: "User")
+          ],
+          initialSelection: 0,
+          key: bottomNavigationKey,
+          onTabChangedListener: (position) {
+            setState(() {
+              currentPage = position;
+            });
+          },
+        ));
   }
 }
