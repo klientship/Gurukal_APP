2dd709c3c77dd844314e231add076fef887d567d
harshithva <harshith11032001@gmail.com>
2022-01-02 20:31:48 +0530

    logo added

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -39,121 +39,107 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        resizeToAvoidBottomPadding: false,
+        // resizeToAvoidBottomPadding: false,
         body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                    child: Text('Hello',
-                        style: TextStyle(
-                            fontSize: 80.0, fontWeight: FontWeight.bold)),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(16.0, 175.0, 0.0, 0.0),
-                    child: Text('There',
-                        style: TextStyle(
-                            fontSize: 80.0, fontWeight: FontWeight.bold)),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
-                    child: Text('.',
-                        style: TextStyle(
-                            fontSize: 80.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.green)),
-                  )
-                ],
-              ),
-            ),
-            Container(
-                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                child: Column(
-                  children: <Widget>[
-                    isError ? Text("Invalid email or password") : Container(),
-                    TextField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                          labelText: 'EMAIL',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.green))),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Image(
+                    image: NetworkImage(
+                        'https://i.ibb.co/WFdrW4M/Logo-Color-Text-Below.jpg'),
+                  )),
+            ],
+          ),
+        ),
+        Container(
+            padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: <Widget>[
+                isError ? Text("Invalid email or password") : Container(),
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                      labelText: 'EMAIL',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green))),
+                ),
+                SizedBox(height: 20.0),
+                TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                      labelText: 'PASSWORD',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green))),
+                  obscureText: true,
+                ),
+                SizedBox(height: 5.0),
+                Container(
+                  alignment: Alignment(1.0, 0.0),
+                  padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                  child: InkWell(
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat',
+                          decoration: TextDecoration.underline),
                     ),
-                    SizedBox(height: 20.0),
-                    TextField(
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                          labelText: 'PASSWORD',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.green))),
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 5.0),
-                    Container(
-                      alignment: Alignment(1.0, 0.0),
-                      padding: EdgeInsets.only(top: 15.0, left: 20.0),
-                      child: InkWell(
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Container(
+                  height: 40.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.greenAccent,
+                    color: Colors.green,
+                    elevation: 7.0,
+                    child: FlatButton(
+                      onPressed: () async {
+                        final String email = emailController.text;
+                        final String password = passwordController.text;
+
+                        final UserModel user = await loginUser(email, password);
+
+                        setState(() {
+                          _user = user;
+                        });
+                        if (isLogin) {
+                          Navigator.pushNamed(context, '/dashboard',
+                              arguments: _user);
+                        }
+                      },
+                      child: Center(
                         child: Text(
-                          'Forgot Password',
+                          'LOGIN',
                           style: TextStyle(
-                              color: Colors.green,
+                              color: Colors.white,
                               fontWeight: FontWeight.bold,
-                              fontFamily: 'Montserrat',
-                              decoration: TextDecoration.underline),
+                              fontFamily: 'Montserrat'),
                         ),
                       ),
                     ),
-                    SizedBox(height: 40.0),
-                    Container(
-                      height: 40.0,
-                      child: Material(
-                        borderRadius: BorderRadius.circular(20.0),
-                        shadowColor: Colors.greenAccent,
-                        color: Colors.green,
-                        elevation: 7.0,
-                        child: FlatButton(
-                          onPressed: () async {
-                            final String email = emailController.text;
-                            final String password = passwordController.text;
-
-                            final UserModel user =
-                                await loginUser(email, password);
-
-                            setState(() {
-                              _user = user;
-                            });
-                            if (isLogin) {
-                              Navigator.pushNamed(context, '/dashboard',
-                                  arguments: _user);
-                            }
-                          },
-                          child: Center(
-                            child: Text(
-                              'LOGIN',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat'),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20.0),
-                  ],
-                )),
-            SizedBox(height: 15.0),
-          ],
-        ));
+                  ),
+                ),
+                SizedBox(height: 20.0),
+              ],
+            )),
+        SizedBox(height: 15.0),
+      ],
+    ));
   }
 }
